2a9bad99d186792e8bf399bf22701a843271cbaf
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-06-08 03:20:28 +0000

    Modificando circular_queue.h

--- include/circular_queue.h
+++ include/circular_queue.h
@@ -1,31 +1,14 @@
 #ifndef CIRCULAR_QUEUE_H
 #define CIRCULAR_QUEUE_H
 
-#include "process.h"
-
 typedef struct CircularQueue CircularQueue;
 
-CircularQueue* cq_create();
-
-void cq_enqueue(
-    CircularQueue* queue,
-    Process* process
-);
-
-Process* cq_dequeue(
-    CircularQueue* queue
-);
-
-Process* cq_current(
-    CircularQueue* queue
-);
-
-int cq_is_empty(
-    CircularQueue* queue
-);
-
-void cq_destroy(
-    CircularQueue* queue
-);
+CircularQueue* cq_create(int capacity);
+void cq_enqueue(CircularQueue* queue, int value);
+int cq_dequeue(CircularQueue* queue);
+int cq_front(CircularQueue* queue);
+int cq_is_empty(CircularQueue* queue);
+int cq_is_full(CircularQueue* queue);
+void cq_destroy(CircularQueue* queue);
 
 #endif
