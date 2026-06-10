55a3643347575b24a4ca0bc30d4bdcd829ff7746
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-06-10 07:00:27 +0000

    Añadiendo greedy.c

---
+++ src/algorithms/greedy.c
@@ -0,0 +1,20 @@
+#include "algorithms.h"
+
+/*
+Algoritmos tipo greedy usados en el proyecto:
+
+- First Fit
+- Best Fit
+- Worst Fit
+- Shortest Job First (SJF)
+
+Están implementados en estos archivos:
+
+src/memory/
+    first_fit.c
+    best_fit.c
+    worst_fit.c
+
+src/scheduler/
+    sjf.c
+*/
