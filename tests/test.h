eb72b9b86d14215795ba3ca5197072fdeda04131
Esquivel Cortés Iker Alonso <ikering226@gmail.com>
2026-06-10 02:04:30 +0000

    Creando test.h

---
+++ tests/test.h
@@ -0,0 +1,20 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+/* Stack */
+void test_stack_push_pop();
+void test_stack_empty();
+
+/* Queue */
+void test_queue_fifo();
+
+/* Circular Queue */
+void test_round_robin_behavior();
+
+/* Scheduler */
+void test_fifo_scheduler();
+
+/* Memory Manager */
+void test_first_fit();
+
+#endif
